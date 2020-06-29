834c8e739504f01f6480e84525cdb7a32ad28c0b
Squares <m.piotr@moscicki.pl>
2020-06-29 20:39:42 +0200

    Implement child removal

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 pub mod poly_ui {
-    use std::{
-        rc::Rc,
-        fmt::Debug,
-    };
+    use std::{fmt::Debug, rc::Rc};
 
     #[derive(Debug)]
     pub struct Hierarchy {
         children: Vec<Rc<dyn Widget>>,
     }
-    
+
     impl Hierarchy {
         pub fn new() -> Self {
-            return Self{
+            return Self {
                 children: Vec::new(),
             };
         }
@@ -20,12 +17,21 @@ pub mod poly_ui {
             self.children.push(child);
         }
 
+        pub fn remove(&mut self, child: &dyn Widget) {
+            self.children.remove(
+                self.children
+                    .iter()
+                    .position(|elem| std::ptr::eq(elem.as_ref(), child))
+                    .unwrap(),
+            );
+        }
+
         pub fn children(&self) -> &Vec<Rc<dyn Widget>> {
             return &self.children;
         }
     }
 
-    pub trait Widget : Debug {
+    pub trait Widget: Debug {
         fn hierarchy(&self) -> &Hierarchy;
     }
 
@@ -38,17 +44,17 @@ pub mod poly_ui {
             hierarchy: Hierarchy,
         }
 
-        impl Widget for TestWidget {
-            fn hierarchy(&self) -> &Hierarchy {
-                return &self.hierarchy;
+        impl TestWidget {
+            fn new() -> Self {
+                return Self {
+                    hierarchy: Hierarchy::new(),
+                };
             }
         }
 
-        impl TestWidget {
-            fn new() -> Self {
-                return Self{
-                    hierarchy: Hierarchy::new(),
-                };
+        impl Widget for TestWidget {
+            fn hierarchy(&self) -> &Hierarchy {
+                return &self.hierarchy;
             }
         }
 
@@ -58,7 +64,31 @@ pub mod poly_ui {
             let child_widget = Rc::new(TestWidget::new());
 
             parent_widget.hierarchy.add(child_widget.clone());
-            assert_eq!(std::ptr::eq(parent_widget.hierarchy().children()[0].as_ref(), child_widget.as_ref()), true);
+            assert_eq!(
+                std::ptr::eq(
+                    parent_widget.hierarchy().children()[0].as_ref(),
+                    child_widget.as_ref()
+                ),
+                true
+            );
+        }
+
+        #[test]
+        fn remove_child() {
+            let mut parent_widget = TestWidget::new();
+            let child_widget_1 = Rc::new(TestWidget::new());
+            let child_widget_2 = Rc::new(TestWidget::new());
+
+            parent_widget.hierarchy.add(child_widget_1.clone());
+            parent_widget.hierarchy.add(child_widget_2.clone());
+            parent_widget.hierarchy.remove(child_widget_1.as_ref());
+            assert_eq!(
+                std::ptr::eq(
+                    parent_widget.hierarchy().children()[0].as_ref(),
+                    child_widget_2.as_ref()
+                ),
+                true
+            );
         }
     }
 }

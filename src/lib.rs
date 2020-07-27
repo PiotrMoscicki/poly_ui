76a61447b83ac392f1ba24f847f2a02093525476
Piotr Mościcki <m.piotr@moscicki.pl>
2020-07-27 05:18:02 +0000

    Add BaseWidget
    - Share hierarchy between Widget and Layout instead of storing pointer
    to the widget in layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 pub mod poly_ui {
     use nalgebra::Vector3;
     use uuid::Uuid;
-    use std::{collections::HashMap, fmt::Debug, rc::{Rc, Weak}};
+    use std::{collections::HashMap, fmt::Debug, rc::{Rc, Weak}, cell::RefCell};
 
     // traits
     //********************************************************************************************
     //********************************************************************************************
     //********************************************************************************************
     pub trait Layout: Debug {
-        fn set_owner(&mut self, owner: Owner);
-        fn owner(&self) -> &Owner;
-
-        fn add(&mut self, child: Rc<dyn Widget>, pos: Vector3<i32>);
+        fn add(&mut self, child: Rc<RefCell<dyn Widget>>, pos: Vector3<i32>);
     }
 
     //********************************************************************************************
@@ -45,15 +42,63 @@ pub mod poly_ui {
     //********************************************************************************************
     //********************************************************************************************
     //********************************************************************************************
+    
+    //********************************************************************************************
+    #[derive(Debug)]
+    pub struct BaseWidget {
+        id: Uuid,
+        pos: Vector3<i32>,
+        hierarchy: Hierarchy,
+        layout: Box<dyn Layout>,
+    }
+    
+    impl BaseWidget {
+        pub fn new() -> Self {
+            return Self {
+                id: Uuid::new_v4(),
+                pos: Vector3::<i32>::new(0, 0, 0),
+                hierarchy: Hierarchy::new(),
+                layout: Box::new(CanvasLayout::new()),
+            };
+        }
+    }
+    
+    impl Widget for BaseWidget {
+        fn id(&self) -> &Uuid {
+            return &self.id;
+        }
+        fn pos(&self) -> &Vector3<i32> {
+            return &self.pos;
+        }
+        fn hierarchy(&self) -> &Hierarchy {
+            return &self.hierarchy;
+        }
+        fn hierarchy_mut(&mut self) -> &mut Hierarchy {
+            return &mut self.hierarchy;
+        }
+        fn set_layout(&mut self, layout: Box<dyn Layout>) {
+            layout.set_owner(owner: Owner)
+            self.layout = layout;
+        }
+        fn layout(&self) -> &dyn Layout {
+            return self.layout.as_ref();
+        }
+        fn layout_mut(&mut self) -> &mut dyn Layout {
+            return self.layout.as_mut();
+        }
+    }
+
+    //********************************************************************************************
     #[derive(Debug)]
     pub struct Transform {
         pos: Vector3<i32>,
         size: Vector3<i32>,
     }
 
+    //********************************************************************************************
     #[derive(Debug)]
     pub struct Hierarchy {
-        children: Vec<Rc<dyn Widget>>,
+        children: Vec<Rc<RefCell<dyn Widget>>>,
     }
 
     impl Hierarchy {
@@ -63,20 +108,20 @@ pub mod poly_ui {
             };
         }
 
-        pub fn add(&mut self, child: Rc<dyn Widget>) {
+        pub fn add(&mut self, child: Rc<RefCell<dyn Widget>>) {
             self.children.push(child);
         }
 
-        pub fn remove(&mut self, child: &dyn Widget) {
+        pub fn remove(&mut self, child: &Rc<RefCell<dyn Widget>>) {
             self.children.remove(
                 self.children
                     .iter()
-                    .position(|elem| std::ptr::eq(elem.as_ref(), child))
+                    .position(|elem| elem.borrow().id() == child.borrow().id())
                     .unwrap(),
             );
         }
 
-        pub fn children(&self) -> &Vec<Rc<dyn Widget>> {
+        pub fn children(&self) -> &Vec<Rc<RefCell<dyn Widget>>> {
             return &self.children;
         }
     }
@@ -84,20 +129,20 @@ pub mod poly_ui {
     //********************************************************************************************
     #[derive(Debug)]
     pub struct Owner {
-        owner: Weak<dyn Widget>,
+        data: Weak<RefCell<dyn Widget>>,
     }
 
     //********************************************************************************************
     #[derive(Debug)]
     pub struct CanvasLayout {
         owner: Owner,
-        children: HashMap<Rc<dyn Widget>, Vector3<i32>>,
+        children: HashMap<Uuid, Vector3<i32>>,
     }
 
     impl CanvasLayout {
         pub fn new() -> Self {
             return Self {
-                owner: Owner{ owner: Weak::<dyn Widget>::new() },
+                owner: Owner{ data: Weak::<RefCell<BaseWidget>>::new() },
                 children: HashMap::new(),
             };
         }
@@ -112,8 +157,9 @@ pub mod poly_ui {
             return &self.owner;
         }
 
-        fn add(&mut self, child: Rc<dyn Widget>, pos: Vector3<i32>) {
-            self.children.insert(child, pos);
+        fn add(&mut self, child: Rc<RefCell<dyn Widget>>, pos: Vector3<i32>) {
+            self.children.insert(*child.borrow().id(), pos);
+            self.owner.data.upgrade().unwrap().borrow_mut().hierarchy_mut().add(child);
         }
     }
 
@@ -125,100 +171,44 @@ pub mod poly_ui {
     mod tests {
         use super::*;
 
-        // test structs
-        //****************************************************************************************
-        //****************************************************************************************
-        //****************************************************************************************
-        #[derive(Debug)]
-        struct TestWidget {
-            id: Uuid,
-            pos: Vector3<i32>,
-            hierarchy: Hierarchy,
-            layout: Box<dyn Layout>,
-        }
-
-        //****************************************************************************************
-        impl TestWidget {
-            fn new() -> Self {
-                return Self {
-                    id: Uuid::new_v4(),
-                    pos: Vector3::<i32>::new(0, 0, 0),
-                    hierarchy: Hierarchy::new(),
-                    layout: Box::new(CanvasLayout::new()),
-                };
-            }
-        }
-
-        //****************************************************************************************
-        impl Widget for TestWidget {
-            fn id(&self) -> &Uuid {
-                return &self.id;
-            }
-
-            fn pos(&self) -> &Vector3<i32> {
-                return &self.pos;
-            }
-            fn hierarchy(&self) -> &Hierarchy {
-                return &self.hierarchy;
-            }
-
-            fn hierarchy_mut(&mut self) -> &mut Hierarchy {
-                return &mut self.hierarchy;
-            }
-
-            fn set_layout(&mut self, layout: Box<dyn Layout>) {
-                self.layout = layout;
-            }
-
-            fn layout(&self) -> &dyn Layout {
-                return self.layout.as_ref();
-            }
-
-            fn layout_mut(&mut self) -> &mut dyn Layout {
-                return self.layout.as_mut();
-            }
-        }
-
         // test cases
         //****************************************************************************************
         //****************************************************************************************
         //****************************************************************************************
         #[test]
         fn widget_add_child() {
-            let mut parent_widget = TestWidget::new();
-            let child_widget = Rc::new(TestWidget::new());
+            let mut parent_widget = BaseWidget::new();
+            let child_widget = Rc::new(RefCell::new(BaseWidget::new()));
 
             parent_widget.hierarchy_mut().add(child_widget.clone());
 
-            assert_eq!(parent_widget.hierarchy().children()[0].id(), child_widget.id());
+            assert_eq!(parent_widget.hierarchy().children()[0].borrow().id(), child_widget.borrow().id());
         }
 
         //****************************************************************************************
         #[test]
         fn widget_remove_child() {
-            let mut parent_widget = TestWidget::new();
-            let child_widget_1 = Rc::new(TestWidget::new());
-            let child_widget_2 = Rc::new(TestWidget::new());
+            let mut parent_widget = BaseWidget::new();
+            let child_widget_1 = Rc::new(RefCell::new(BaseWidget::new())) as Rc<RefCell<dyn Widget>>;
+            let child_widget_2 = Rc::new(RefCell::new(BaseWidget::new())) as Rc<RefCell<dyn Widget>>;
 
             parent_widget.hierarchy_mut().add(child_widget_1.clone());
             parent_widget.hierarchy_mut().add(child_widget_2.clone());
-            parent_widget
-                .hierarchy_mut()
-                .remove(child_widget_1.as_ref());
+            parent_widget.hierarchy_mut().remove(&child_widget_1);
 
-            assert_eq!(parent_widget.hierarchy().children()[0].id(), child_widget_2.id());
+            assert_eq!(parent_widget.hierarchy().children()[0].borrow().id(), child_widget_2.borrow().id());
         }
 
         //****************************************************************************************
         #[test]
         fn canvas_layout_add_child() {
-            let mut parent_widget = TestWidget::new();
+            let mut parent_widget = BaseWidget::new();
             parent_widget.set_layout(Box::new(CanvasLayout::new()));
-            let child_widget = Rc::new(TestWidget::new());
+            let child_widget = Rc::new(RefCell::new(BaseWidget::new()));
 
             parent_widget.layout_mut().add(child_widget.clone(), Vector3::<i32>::new(1, 2, 0));
             
-            assert_eq!(parent_widget.hierarchy().children()[0].id(), child_widget.id());
+            assert_eq!(parent_widget.hierarchy().children()[0].borrow().id(), child_widget.borrow().id());
         }
     }
 }

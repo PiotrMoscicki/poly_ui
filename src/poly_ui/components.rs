7021499b16a2e1fe76d7a2694382ecb5eacbf5b2
Piotr Mościcki <m.piotr@moscicki.pl>
2020-07-30 08:42:10 +0000

    Move add/remove child tests to components module

--- src/poly_ui/components.rs
+++ src/poly_ui/components.rs
@@ -42,3 +42,41 @@ impl Hierarchy {
         return &self.children;
     }
 }
+
+// tests
+//********************************************************************************************
+//********************************************************************************************
+//********************************************************************************************
+#[cfg(test)]
+mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
+    use crate::poly_ui::widgets::{BaseWidget, Widget};
+
+    //****************************************************************************************
+    #[test]
+    fn hierarchy_add_child() {
+        let mut parent_widget = BaseWidget::new();
+        let child_widget = Rc::new(RefCell::new(BaseWidget::new()));
+        parent_widget.hierarchy_mut().add(child_widget.clone());
+        assert_eq!(
+            parent_widget.hierarchy().children()[0].borrow().id(),
+            child_widget.borrow().id()
+        );
+    }
+
+    //****************************************************************************************
+    #[test]
+    fn hierarchy_remove_child() {
+        let mut parent_widget = BaseWidget::new();
+        let child_widget_1 = Rc::new(RefCell::new(BaseWidget::new())) as Rc<RefCell<dyn Widget>>;
+        let child_widget_2 = Rc::new(RefCell::new(BaseWidget::new())) as Rc<RefCell<dyn Widget>>;
+        parent_widget.hierarchy_mut().add(child_widget_1.clone());
+        parent_widget.hierarchy_mut().add(child_widget_2.clone());
+        parent_widget.hierarchy_mut().remove(&child_widget_1);
+        assert_eq!(
+            parent_widget.hierarchy().children()[0].borrow().id(),
+            child_widget_2.borrow().id()
+        );
+    }
+}
--- src/poly_ui/widgets.rs
+++ src/poly_ui/widgets.rs
@@ -15,9 +15,12 @@ use crate::poly_ui::layouts::{CanvasLayout, Layout};
 //************************************************************************************************
 pub trait Widget: Debug {
     fn id(&self) -> &Uuid;
+
     fn pos(&self) -> &Vector3<i32>;
+
     fn hierarchy(&self) -> Ref<Hierarchy>;
     fn hierarchy_mut(&mut self) -> RefMut<Hierarchy>;
+
     fn set_layout(&mut self, layout: Box<dyn Layout>);
     fn layout(&self) -> &dyn Layout;
     fn layout_mut(&mut self) -> &mut dyn Layout;
@@ -89,43 +92,5 @@ impl Widget for BaseWidget {
 
     fn layout_mut(&mut self) -> &mut dyn Layout {
         return self.layout.as_mut();
-    }
-}
-
-// tests
-//********************************************************************************************
-//********************************************************************************************
-//********************************************************************************************
-#[cfg(test)]
-mod tests {
-    use std::{cell::RefCell, rc::Rc};
-
-    use crate::poly_ui::widgets::{BaseWidget, Widget};
-
-    //****************************************************************************************
-    #[test]
-    fn widget_add_child() {
-        let mut parent_widget = BaseWidget::new();
-        let child_widget = Rc::new(RefCell::new(BaseWidget::new()));
-        parent_widget.hierarchy_mut().add(child_widget.clone());
-        assert_eq!(
-            parent_widget.hierarchy().children()[0].borrow().id(),
-            child_widget.borrow().id()
-        );
-    }
-
-    //****************************************************************************************
-    #[test]
-    fn widget_remove_child() {
-        let mut parent_widget = BaseWidget::new();
-        let child_widget_1 = Rc::new(RefCell::new(BaseWidget::new())) as Rc<RefCell<dyn Widget>>;
-        let child_widget_2 = Rc::new(RefCell::new(BaseWidget::new())) as Rc<RefCell<dyn Widget>>;
-        parent_widget.hierarchy_mut().add(child_widget_1.clone());
-        parent_widget.hierarchy_mut().add(child_widget_2.clone());
-        parent_widget.hierarchy_mut().remove(&child_widget_1);
-        assert_eq!(
-            parent_widget.hierarchy().children()[0].borrow().id(),
-            child_widget_2.borrow().id()
-        );
     }
 }

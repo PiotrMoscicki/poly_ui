e9dab4bff102bc3b9fa705b583d40687417f08a7
Piotr Mościcki <m.piotr@moscicki.pl>
2020-10-24 18:03:35 +0200

    Working showing window test

--- src/main.rs
+++ src/main.rs
@@ -92,17 +92,30 @@ fn run(font_path: &Path) -> Result<(), String> {
     Ok(())
 }
 
+pub mod poly_ui {
+    pub mod app;
+
+    pub mod components;
+    pub mod layouts;
+    pub mod widgets;
+    
+    pub mod sdl2;
+}
+use crate::poly_ui::app::AppTrait;
+
 fn main() -> Result<(), String> {
-    let args: Vec<_> = env::args().collect();
+    // let args: Vec<_> = env::args().collect();
 
-    println!("linked sdl2_ttf: {}", sdl2::ttf::get_linked_version());
+    // println!("linked sdl2_ttf: {}", sdl2::ttf::get_linked_version());
 
-    if args.len() < 2 {
-        println!("Usage: ./demo font.[ttf|ttc|fon]")
-    } else {
-        let path: &Path = Path::new(&args[1]);
-        run(path)?;
-    }
+    // if args.len() < 2 {
+    //     println!("Usage: ./demo font.[ttf|ttc|fon]")
+    // } else {
+    //     let path: &Path = Path::new(&args[1]);
+    //     run(path)?;
+    // }
 
-    Ok(())
+    let mut app = poly_ui::sdl2::App::new();
+    let mut window = app.get_windows_manager().create_window("dupaaa", 800, 600);
+    return app.exec();
 }

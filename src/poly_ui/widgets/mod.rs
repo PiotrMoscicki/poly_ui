d2adc060e9bfc5300aa29fb77961101e428376ab
Piotr Mościcki <m.piotr@moscicki.pl>
2020-10-24 13:21:25 +0200

    Add Window definition prototype

--- src/poly_ui/widgets/mod.rs
+++ src/poly_ui/widgets/mod.rs
@@ -1,5 +1,9 @@
 mod widget_trait;
+mod window_trait;
 mod widget;
+mod window;
 
 pub use widget_trait::WidgetTrait;
+pub use window_trait::WindowTrait;
 pub use widget::Widget;
+pub use window::Window;
---
+++ src/poly_ui/widgets/window.rs
@@ -0,0 +1,66 @@
+use nalgebra::Vector2;
+use std::{
+    cell::{Ref, RefMut},
+    fmt::Debug,
+};
+use uuid::Uuid;
+
+use crate::poly_ui::components::Hierarchy;
+use crate::poly_ui::layouts::Layout;
+
+use super::widget_trait::WidgetTrait;
+use super::window_trait::WindowTrait;
+use super::widget::Widget;
+
+//************************************************************************************************
+//************************************************************************************************
+//************************************************************************************************
+#[derive(Debug)]
+pub struct Window {
+    base: Widget,
+}
+
+//************************************************************************************************
+impl Window {
+    pub fn new() -> Self {
+        return Self {
+            base: Widget::new(),
+        };
+    }
+}
+
+//************************************************************************************************
+impl WidgetTrait for Window {
+    fn id(&self) -> &Uuid {
+        return self.base.id();
+    }
+
+    fn pos(&self) -> &Vector2<i32> {
+        return self.base.pos();
+    }
+
+    fn hierarchy(&self) -> Ref<Hierarchy> {
+        return self.base.hierarchy();
+    }
+
+    fn hierarchy_mut(&mut self) -> RefMut<Hierarchy> {
+        return self.base.hierarchy_mut();
+    }
+
+    fn set_layout(&mut self, layout: Box<dyn Layout>) {
+        self.base.set_layout(layout);
+    }
+
+    fn layout(&self) -> &dyn Layout {
+        return self.base.layout();
+    }
+
+    fn layout_mut(&mut self) -> &mut dyn Layout {
+        return self.base.layout_mut();
+    }
+}
+
+//************************************************************************************************
+impl WindowTrait for Window {
+
+}

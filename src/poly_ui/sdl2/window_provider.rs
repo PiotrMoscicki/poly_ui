ee3ab6189d63ac1753fc7deea79ccd656fd2e98b
Piotr Mościcki <m.piotr@moscicki.pl>
2020-10-24 14:25:42 +0200

    Add empty SDL2 WindowProvider

---
+++ src/poly_ui/sdl2/window_provider.rs
@@ -0,0 +1,11 @@
+use crate::poly_ui::widgets::WindowProviderTrait
+
+pub struct WindowProvider {
+
+}
+
+impl WindowProvider {
+    pub fn new(title: &str, width: i32, height: i32) -> Self {
+        return WindowProvider{};
+    }
+}

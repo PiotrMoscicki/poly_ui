dd4e1b8ab51bea04bb2be392e9f64fbb6d949430
Piotr Mościcki <m.piotr@moscicki.pl>
2020-12-30 10:48:02 +0100

    Some changes

--- src/poly_ui/widgets/layout_items_size_gen.rs
+++ src/poly_ui/widgets/layout_items_size_gen.rs
@@ -3,10 +3,41 @@
 //************************************************************************************************
 #[derive(Debug)]
 pub struct Item {
-    pub stretch: u32, // any value
-    pub min_size: u32, // any value
-    pub max_size: u32, // ensured to be higher or equal min_size
-    pub current_size: u32, // ensured to have at least one element
+    stretch: u32, // any value
+    min_size: u32, // any value
+    max_size: u32, // ensured to be higher or equal min_size
+    current_size: u32,
+}
+
+impl Item {
+    pub fn new(stretch: u32, min_size: Option<u32>, max_size: Option<u32>) -> Self {
+        return Self{
+            stretch,
+            min_size: min_size.unwrap_or(0),
+            max_size: max_size.unwrap_or(u32::MAX),
+            current_size: 0,
+        }
+    }
+
+    pub fn get_stretch(&self) -> u32 {
+        return self.stretch;
+    }
+
+    pub fn get_min_size(&self) -> u32 {
+        return self.min_size;
+    }
+
+    pub fn get_max_size(&self) -> u32 {
+        return self.max_size;
+    }
+
+    pub fn get_current_size(&self) -> u32 {
+        return self.current_size;
+    }
+
+    fn get_max_minus_current(&seld) -> u32 {
+        return self.max_size - self.current_size;
+    }
 }
 
 //************************************************************************************************
@@ -15,44 +46,63 @@ pub struct Item {
 #[derive(Debug)]
 pub struct Layout {
     pub size: u32, // size is large enough to fit all items in their lowest sizes
-    pub items: Vec<Item>,
+    pub items: Vec<Item>, // can be empty
 }
 
 //************************************************************************************************
 impl Layout {
-    pub fn validate(&mut self) {
+    pub fn new(size: u32, items: Vec<Item>) -> Self {
+        let mut result = Self{
+            size,
+            items,
+        };
+
+        result.ensure_layout_has_at_least_minimal_width();
+        result.validate_all_items();
+
+        result.validate();
+
+        return result;
+    }
+
+    pub fn get_size(&self) -> u32 {
+        return self.size;
+    }
+
+    pub fn get_items(&self) -> &Vec<Item> {
+        return &self.items;
+    }
+
+    fn validate(&mut self) {
         if self.items.len() > 0 {
-            self.ensure_layout_has_at_least_minimal_width();
-            self.validate_all_items();
-            
             let items_stretch = self.gather_items_stretch();
-            let item_with_lowest_current_max_diff = self.get_item_with_lowest_current_max_diff();
-            let lowest_current_max_diff = 
-                self.items[item_with_lowest_current_max_diff].get_current_max_diff();
+            let item_with_lowest_max_minus_current = self.get_item_with_lowest_max_minus_current();
+            let lowest_max_minus_current = 
+                self.items[item_with_lowest_max_minus_current].get_max_minus_current();
             let remaining_free_layout_space = self.remaining_free_layout_space();
             
             if remaining_free_layout_space == 0 {
                 return;
             }
-            else if remaining_free_layout_space < lowest_current_max_diff * items_stretch {
+            else if remaining_free_layout_space < lowest_max_minus_current * items_stretch {
                 self.set_every_item_size_to_at_least_min();
                 self.increase_every_item_size(remaining_free_layout_space / items_stretch);
                 let remainder = remaining_free_layout_space % items_stretch;
                 for _ in 0..remainder {
-                    let highest_diff = self.get_item_with_highest_current_vs_expected_stretch_diff();
+                    let highest_diff = self.get_item_with_highest_expected_minus_current_stretch();
                     self.items[highest_diff].current_size += 1;
                 }
             }
             else {
-                self.increase_every_item_size(lowest_current_max_diff);
+                self.increase_every_item_size(lowest_max_minus_current);
                 
-                let mut item = self.items.remove(item_with_lowest_current_max_diff);
+                let mut item = self.items.remove(item_with_lowest_max_minus_current);
                 self.size -= item.current_size;
 
                 self.validate();
                 
                 self.size += item.current_size;
-                self.items.insert(item_with_lowest_current_max_diff, item);
+                self.items.insert(item_with_lowest_max_minus_current, item);
             }
         }
         else {
@@ -90,7 +140,7 @@ impl Layout {
         }
     }
 
-    fn get_item_with_lowest_current_max_diff(&mut self) -> usize {
+    fn get_item_with_lowest_max_minus_current(&mut self) -> usize {
         let mut idx = 0;
         let mut lowest_idx = 0;
         let mut lowest_diff = self.items[0].max_size - self.items[0].min_size;
@@ -131,15 +181,29 @@ impl Layout {
         }
     }
 
-    fn get_item_with_highest_current_vs_expected_stretch_diff(&self) -> usize {
-        let mut highest_idx = 0;
-        //let mut highest_diff = 
-        // stretch_value = stretch / total_stretch
-        // current_stretch_value = size / total_size
+    fn get_item_with_highest_expected_minus_current_stretch(&self) -> usize {
+        let total_stretch = self.gather_items_stretch();
         
-        // stretch / total_stretch = size / total_size
-        // stretch * total_size = size * total_stretch
-        //
+        let mut highest_idx = 0;
+        let mut highest_diff = 0;
+        let mut idx = 0;
+        for item in self.items {
+            let potential_highest = self::get_item_expected_minus_current_stretch(item, total_stretch, self.size);
+
+            if potential_highest > highest_diff {
+                highest_idx = idx;
+                highest_diff = potential_highest;
+            }
+
+            idx += 1;
+        }
+
+        return highest_idx;
+    }
+
+    fn get_item_expected_minus_current_stretch(item: &Item, total_stretch: u32, 
+        total_size: u32) -> u32 {
+        return item.stretch * total_size - item.current_size * total_stretch;
     }
 }
 
@@ -150,104 +214,244 @@ mod tests {
 
     //********************************************************************************************
     #[test]
-    fn validated_item_update_max_size() {
-        let mut item = ValidatedItem{
-            stretch: 0,
-            min_size: 0,
-            max_size: u32::MAX,
-            current_size: 0,
-        };
-        item.update_max_size(120, 0, 4);
-        assert_eq!(item.max_size, 30);
+    fn ensure_layout_has_at_least_minimal_width() {
+        {
+            let mut layout = Layout{
+                size: 0,
+                items: vec!(
+                    Item{
+                        stretch: 0,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 0,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 0,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                )
+            };
+            layout.ensure_layout_has_at_least_minimal_width();
+            assert_eq!(layout.size, 0);
+        }
+        {
+            let mut layout = Layout{
+                size: 0,
+                items: vec!(
+                    Item{
+                        stretch: 0,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 0,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 0,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                )
+            };
+            layout.ensure_layout_has_at_least_minimal_width();
+            assert_eq!(layout.size, 0);
+        }
+        {
+            let mut layout = Layout{
+                size: 0,
+                items: vec!(
+                    Item{
+                        stretch: 5,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 5,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 1,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                )
+            };
+            layout.ensure_layout_has_at_least_minimal_width();
+            assert_eq!(layout.size, 0);
+        }
+        {
+            let mut layout = Layout{
+                size: 0,
+                items: vec!(
+                    Item{
+                        stretch: 5,
+                        min_size: 15,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 5,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 1,
+                        min_size: 5,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                )
+            };
+            layout.ensure_layout_has_at_least_minimal_width();
+            assert_eq!(layout.size, 20);
+        }
+        {
+            let mut layout = Layout{
+                size: 0,
+                items: vec!(
+                    Item{
+                        stretch: 5,
+                        min_size: 15,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 5,
+                        min_size: 0,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                    Item{
+                        stretch: 1,
+                        min_size: 5,
+                        max_size: 0,
+                        current_size: 0,
+                    },
+                )
+            };
+            layout.ensure_layout_has_at_least_minimal_width();
+            assert_eq!(layout.size, 40);
+        }
     }
 
-    //********************************************************************************************
-    #[test]
-    fn input_item_update_max_size() {
-        let mut item = InputItem{
-            stretch: 0,
-            min_size: 0,
-            max_size: u32::MAX,
-        };
-        item.update_max_size(120, 0, 4);
-        assert_eq!(item.max_size, 30);
-    }
+    // //********************************************************************************************
+    // #[test]
+    // fn validated_item_update_max_size() {
+    //     let mut item = ValidatedItem{
+    //         stretch: 0,
+    //         min_size: 0,
+    //         max_size: u32::MAX,
+    //         current_size: 0,
+    //     };
+    //     item.update_max_size(120, 0, 4);
+    //     assert_eq!(item.max_size, 30);
+    // }
 
-    //********************************************************************************************
-    #[test]
-    fn no_items() {
-        let input = InputLayout{
-            size: 120,
-            items: vec!{},
-        };
-        let validated = input.validate();
-        assert_eq!(validated.is_none(), true);
-    }
+    // //********************************************************************************************
+    // #[test]
+    // fn input_item_update_max_size() {
+    //     let mut item = InputItem{
+    //         stretch: 0,
+    //         min_size: 0,
+    //         max_size: u32::MAX,
+    //     };
+    //     item.update_max_size(120, 0, 4);
+    //     assert_eq!(item.max_size, 30);
+    // }
 
-    //********************************************************************************************
-    #[test]
-    fn no_min_max_stretch() {
-        let input = InputLayout{
-            size: 120,
-            items: vec!{
-                InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
-                InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
-                InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
-                InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
-            },
-        };
-        let mut validated = input.validate().unwrap();
-        validated.calculate();
+    // //********************************************************************************************
+    // #[test]
+    // fn no_items() {
+    //     let input = InputLayout{
+    //         size: 120,
+    //         items: vec!{},
+    //     };
+    //     let validated = input.validate();
+    //     assert_eq!(validated.is_none(), true);
+    // }
 
-        assert_eq!(validated.size, 120);
-        assert_eq!(validated.stretch, 0);
-        assert_eq!(validated.items[0].current_size, 30);
-        assert_eq!(validated.items[1].current_size, 30);
-        assert_eq!(validated.items[2].current_size, 30);
-        assert_eq!(validated.items[3].current_size, 30);
-    }
+    // //********************************************************************************************
+    // #[test]
+    // fn no_min_max_stretch() {
+    //     let input = InputLayout{
+    //         size: 120,
+    //         items: vec!{
+    //             InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
+    //             InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
+    //             InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
+    //             InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
+    //         },
+    //     };
+    //     let mut validated = input.validate().unwrap();
+    //     validated.calculate();
 
-    //********************************************************************************************
-    #[test]
-    fn no_min_max() {
-        let input = InputLayout{
-            size: 120,
-            items: vec!{
-                InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
-                InputItem{stretch: 1, min_size: 0, max_size: u32::MAX},
-                InputItem{stretch: 2, min_size: 0, max_size: u32::MAX},
-                InputItem{stretch: 3, min_size: 0, max_size: u32::MAX},
-            },
-        };
-        let mut validated = input.validate().unwrap();
-        validated.calculate();
+    //     assert_eq!(validated.size, 120);
+    //     assert_eq!(validated.stretch, 0);
+    //     assert_eq!(validated.items[0].current_size, 30);
+    //     assert_eq!(validated.items[1].current_size, 30);
+    //     assert_eq!(validated.items[2].current_size, 30);
+    //     assert_eq!(validated.items[3].current_size, 30);
+    // }
 
-        assert_eq!(validated.size, 120);
-        assert_eq!(validated.stretch, 6);
-        assert_eq!(validated.items[0].current_size, 0);
-        assert_eq!(validated.items[1].current_size, 20);
-        assert_eq!(validated.items[2].current_size, 40);
-        assert_eq!(validated.items[3].current_size, 60);
-    }
+    // //********************************************************************************************
+    // #[test]
+    // fn no_min_max() {
+    //     let input = InputLayout{
+    //         size: 120,
+    //         items: vec!{
+    //             InputItem{stretch: 0, min_size: 0, max_size: u32::MAX},
+    //             InputItem{stretch: 1, min_size: 0, max_size: u32::MAX},
+    //             InputItem{stretch: 2, min_size: 0, max_size: u32::MAX},
+    //             InputItem{stretch: 3, min_size: 0, max_size: u32::MAX},
+    //         },
+    //     };
+    //     let mut validated = input.validate().unwrap();
+    //     validated.calculate();
 
-    //********************************************************************************************
-    #[test]
-    fn milka() {
-        let input = InputLayout{
-            size: 16,
-            items: vec!{
-                InputItem{stretch: 2, min_size: 1, max_size: u32::MAX},
-                InputItem{stretch: 1, min_size: 6, max_size: u32::MAX},
-                InputItem{stretch: 1, min_size: 2, max_size: u32::MAX},
-            },
-        };
-        let mut validated = input.validate().unwrap();
-        validated.calculate();
+    //     assert_eq!(validated.size, 120);
+    //     assert_eq!(validated.stretch, 6);
+    //     assert_eq!(validated.items[0].current_size, 0);
+    //     assert_eq!(validated.items[1].current_size, 20);
+    //     assert_eq!(validated.items[2].current_size, 40);
+    //     assert_eq!(validated.items[3].current_size, 60);
+    // }
 
-        assert_eq!(validated.size, 16);
-        assert_eq!(validated.stretch, 4);
-        assert_eq!(validated.items[0].current_size, 0);
-        assert_eq!(validated.items[1].current_size, 20);
-        assert_eq!(validated.items[2].current_size, 40);
-    }
+    // //********************************************************************************************
+    // #[test]
+    // fn milka() {
+    //     let input = InputLayout{
+    //         size: 16,
+    //         items: vec!{
+    //             InputItem{stretch: 2, min_size: 1, max_size: u32::MAX},
+    //             InputItem{stretch: 1, min_size: 6, max_size: u32::MAX},
+    //             InputItem{stretch: 1, min_size: 2, max_size: u32::MAX},
+    //         },
+    //     };
+    //     let mut validated = input.validate().unwrap();
+    //     validated.calculate();
+
+    //     assert_eq!(validated.size, 16);
+    //     assert_eq!(validated.stretch, 4);
+    //     assert_eq!(validated.items[0].current_size, 0);
+    //     assert_eq!(validated.items[1].current_size, 20);
+    //     assert_eq!(validated.items[2].current_size, 40);
+    // }
 }

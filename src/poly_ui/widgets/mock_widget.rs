2da5b8953031860aa93e34d4d9c2f4c74dfb6376
Piotr Mościcki <m.piotr@moscicki.pl>
2021-02-16 19:36:54 +0100

    Add update test for MockWidget

--- src/poly_ui/widgets/mock_widget.rs
+++ src/poly_ui/widgets/mock_widget.rs
@@ -1,17 +1,13 @@
 // deps
-// use nalgebra::Point2;
-// use nalgebra::Vector2;
 use uuid::Uuid;
-// // crate
-// use crate::poly_ui::app::Color;
-// use crate::poly_ui::app::PainterTrait;
-// use crate::poly_ui::app::Rect;
+// crate
+use crate::poly_ui::app::PainterTrait;
 use crate::poly_ui::components::Hierarchy;
-// use crate::poly_ui::components::Transform;
-// // super
-// use super::NewWidget;
-// use super::Ownerless;
-// use super::WidgetTrait;
+use crate::poly_ui::components::Transform;
+// super
+use super::NewWidget;
+use super::Ownerless;
+use super::WidgetTrait;
 
 //************************************************************************************************
 //************************************************************************************************
@@ -27,47 +23,82 @@ pub struct MockWidget {
 
 //************************************************************************************************
 impl MockWidget {
-    // pub fn new_raw() -> Self {
-    //     Self {
-    //         id: Uuid::new_v4(),
-    //         hierarchy: Hierarchy::default(),
-    //     }
-    // }
+    pub fn new_raw() -> Self {
+        Self {
+            update_call_count: 0,
+            paint_call_count: 0,
+            id: Uuid::new_v4(),
+            hierarchy: Hierarchy::default(),
+        }
+    }
 
-    // pub fn new() -> NewWidget<Self> {
-    //     NewWidget::new(Self::new_raw())
-    // }
+    pub fn new() -> NewWidget<Self> {
+        NewWidget::new(Self::new_raw())
+    }
 }
 
-// //************************************************************************************************
-// impl WidgetTrait for MockWidget {
-//     fn id(&self) -> &Uuid {
-//         &self.id
-//     }
+//************************************************************************************************
+impl WidgetTrait for MockWidget {
+    fn id(&self) -> &Uuid {
+        &self.id
+    }
 
-//     fn add_child(&mut self, child: Ownerless) {
-//         self.hierarchy.add(child);
-//     }
+    fn add_child(&mut self, child: Ownerless) {
+        self.hierarchy.add(child);
+    }
 
-//     fn remove_child(&mut self, child: &Uuid) -> Ownerless {
-//         self.hierarchy.remove(child)
-//     }
+    fn remove_child(&mut self, child: &Uuid) -> Ownerless {
+        self.hierarchy.remove(child)
+    }
 
-//     fn get_hierarchy(&self) -> &Hierarchy {
-//         &self.hierarchy
-//     }
+    fn get_hierarchy(&self) -> &Hierarchy {
+        &self.hierarchy
+    }
 
-//     fn get_child_transform(&self, child: &Uuid) -> &Transform {
-//         self.hierarchy.get_transform(child)
-//     }
+    fn get_child_transform(&self, child: &Uuid) -> &Transform {
+        self.hierarchy.get_transform(child)
+    }
 
-//     fn update(&mut self, dt: f32) {
-//         self.hierarchy.update_children(dt);
-//         update_call_count += 1;
-//     }
+    fn update(&mut self, dt: f32) {
+        self.hierarchy.update_children(dt);
+        self.update_call_count += 1;
+    }
 
-//     fn paint(&self, painter: &mut dyn PainterTrait) {
-//         self.hierarchy.paint_children(painter);
-//         paint_call_count += 1;
-//     }
-// }
+    fn paint(&self, painter: &mut dyn PainterTrait) {
+        self.hierarchy.paint_children(painter);
+
+        let const_self = self as *const Self;
+        let mut mut_self = const_self as *mut Self;
+
+        unsafe {
+            (*mut_self).paint_call_count += 1;
+        }
+    }
+}
+
+//************************************************************************************************
+//************************************************************************************************
+//************************************************************************************************
+#[cfg(test)]
+mod tests {
+    // super
+    use super::*;
+
+    //********************************************************************************************
+    #[test]
+    fn update() {
+        let mock = MockWidget::new();
+
+        mock.borrow_mut().update(0.0);
+        assert_eq!(mock.borrow().update_call_count, 1);
+    }
+
+    //********************************************************************************************
+    // #[test]
+    // fn paint() {
+    //     let mock = MockWidget::new();
+
+    //     mock.borrow_mut().update(0.0);
+    //     assert_eq!(mock.borrow().update_call_count, 1);
+    // }
+}

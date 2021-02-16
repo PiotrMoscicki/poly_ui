d012cced5e8c9192c5d2eceee28241b0dba1948f
Piotr Mościcki <m.piotr@moscicki.pl>
2021-02-16 19:46:19 +0100

    Add MockPainter for PainterTrait

--- src/poly_ui/app/mod.rs
+++ src/poly_ui/app/mod.rs
@@ -5,6 +5,7 @@ mod windows_manager_trait;
 pub use app_trait::AppTrait;
 pub use painter_trait::Color;
 pub use painter_trait::Line;
+pub use painter_trait::MockPainter;
 pub use painter_trait::PainterTrait;
 pub use painter_trait::Rect;
 pub use windows_manager_trait::WindowsManagerTrait;
--- src/poly_ui/app/painter_trait.rs
+++ src/poly_ui/app/painter_trait.rs
@@ -52,3 +52,49 @@ pub trait PainterTrait {
     fn fill_rect(&mut self, rect: Rect);
     fn fill_rects(&mut self, rect: &[Rect]);
 }
+
+//************************************************************************************************
+//************************************************************************************************
+//************************************************************************************************
+pub struct MockPainter {}
+
+impl PainterTrait for MockPainter {
+    fn sub_painter(&self, _transform: &Transform) -> Box<dyn PainterTrait> {
+        Box::new(MockPainter {})
+    }
+
+    fn size(&self) -> Vector2<u32> {
+        Vector2::<u32>::new(0, 0)
+    }
+
+    fn clear(&mut self) {}
+
+    fn present(&mut self) {}
+
+    fn draw_color(&self) -> Color {
+        Color {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 0,
+        }
+    }
+
+    fn set_draw_color(&mut self, _new: &Color) {}
+
+    fn draw_point(&mut self, _point: &Point2<i32>) {}
+
+    fn draw_points(&mut self, _points: &[Point2<i32>]) {}
+
+    fn draw_line(&mut self, _line: &Line) {}
+
+    fn draw_lines(&mut self, _lines: &[Line]) {}
+
+    fn draw_rect(&mut self, _rect: Rect) {}
+
+    fn draw_rects(&mut self, _rect: &[Rect]) {}
+
+    fn fill_rect(&mut self, _rect: Rect) {}
+
+    fn fill_rects(&mut self, _rect: &[Rect]) {}
+}

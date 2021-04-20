004fff55d4f77d8e4d8993e2dad01b8f751cd2be
Squares <m.piotr@moscicki.pl>
2021-04-20 21:05:03 +0200

    Fix clippy and apt-get in GH Actions

--- src/poly_ui/events/mod.rs
+++ src/poly_ui/events/mod.rs
@@ -1,13 +1,13 @@
-mod event;
-mod event_handler;
+// mod event;
+// mod event_handler;
 
-pub use event::Event;
-pub use event::Key;
-pub use event::KeyPressEvent;
-pub use event::KeyReleaseEvent;
-pub use event::MouseButton;
-pub use event::MouseMoveEvent;
-pub use event::MousePressEvent;
-pub use event::MouseReleaseEvent;
+// pub use event::Event;
+// pub use event::Key;
+// pub use event::KeyPressEvent;
+// pub use event::KeyReleaseEvent;
+// pub use event::MouseButton;
+// pub use event::MouseMoveEvent;
+// pub use event::MousePressEvent;
+// pub use event::MouseReleaseEvent;
 
-pub use event_handler::EventHandler;
+// pub use event_handler::EventHandler;

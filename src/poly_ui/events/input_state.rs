31b61a2dcd3f8d950e6897436ed33af621b46449
Squares <m.piotr@moscicki.pl>
2021-04-25 09:52:12 +0200

    WIP implement InputState

--- src/poly_ui/events/input_state.rs
+++ src/poly_ui/events/input_state.rs
@@ -7,6 +7,9 @@ use super::KeyState;
 use super::KeyboardKey;
 use super::MouseButton;
 
+//************************************************************************************************
+//************************************************************************************************
+//************************************************************************************************
 pub struct InputState {
     pub mouse_pos: Point2<u32>,
     pub mouse_diff: Vector2<u32>,
@@ -18,6 +21,7 @@ pub struct InputState {
     pub previous_mouse_button_state: EnumMap<MouseButton, KeyState>,
 }
 
+//************************************************************************************************
 impl InputState {
     // keyboard keys
 
@@ -25,8 +29,16 @@ impl InputState {
         self.current_keyboard_key_state[key] == KeyState::Pressed
     }
 
+    pub fn is_key_released(&self, key: KeyboardKey) -> bool {
+        self.current_keyboard_key_state[key] == KeyState::Released
+    }
+
     pub fn was_key_just_pressed(&self, key: KeyboardKey) -> bool {
         self.is_key_pressed(key) && self.previous_keyboard_key_state[key] == KeyState::Released
+    }
+
+    pub fn was_key_just_released(&self, key: KeyboardKey) -> bool {
+        self.is_key_released(key) && self.previous_keyboard_key_state[key] == KeyState::Pressed
     }
 
     // mouse buttons
@@ -35,8 +47,49 @@ impl InputState {
         self.current_mouse_button_state[button] == KeyState::Pressed
     }
 
+    pub fn is_button_released(&self, button: MouseButton) -> bool {
+        self.current_mouse_button_state[button] == KeyState::Released
+    }
+
     pub fn was_button_just_pressed(&self, button: MouseButton) -> bool {
         self.is_button_pressed(button)
             && self.previous_mouse_button_state[button] == KeyState::Released
+    }
+
+    pub fn was_button_just_released(&self, button: MouseButton) -> bool {
+        self.is_button_released(button)
+            && self.previous_mouse_button_state[button] == KeyState::Pressed
+    }
+}
+
+//************************************************************************************************
+//************************************************************************************************
+//************************************************************************************************
+#[cfg(test)]
+mod tests {
+    // crate
+    use crate::poly_ui::app::MockPainter;
+    // super
+    use super::*;
+
+    //********************************************************************************************
+    #[test]
+    fn update() {
+        let mock = MockWidget::new();
+
+        mock.borrow_mut().update(0.0);
+        assert_eq!(mock.borrow().update_call_count, 1);
+    }
+
+    //********************************************************************************************
+    #[test]
+    fn paint() {
+        let mock = MockWidget::new();
+        let mut painter = MockPainter::default();
+
+        mock.borrow_mut().paint(&mut painter);
+        mock.borrow_mut().paint(&mut painter);
+        mock.borrow_mut().paint(&mut painter);
+        assert_eq!(mock.borrow().paint_call_count, 3);
     }
 }
